e24bf5f271570e5681ee29a47014a3dbedd26962
ZiadGad <ziadamrjad@gmail.com>
2024-09-26 05:38:53 +0300

    Add Error Handling

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const productRouter = require('./routes/productRoutes');
 
 const app = express();
@@ -12,5 +15,11 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use('/api/v1/products', productRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
